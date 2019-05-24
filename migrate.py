6d21a6caae659125234362347640040d65e129db
Jakub Bielecki <jakub.bielecki@codilime.com>
2019-05-24 16:34:31 +0200

    push force

--- migrate.py
+++ migrate.py
@@ -290,11 +290,11 @@ def push(repo, branches, dry_run=True):
     exec(cmd, cwd=path)
     for branch in branches:
         if is_branch_migrated(repo, branch):
-            cmd = ['git', 'push', 'new', branch]
+            cmd = ['git', 'push', '-f', 'new', branch]   # force is impossible on gerrit anyway? --> need separate script for ssh -> gerrit delete branch
             print(cmd)
             if not dry_run:
                 exec(cmd, cwd=path)
-            cmd2 = ['git', 'push', 'new', 'refs/replace/*:refs/replace/*']
+            cmd2 = ['git', 'push', '-f', 'new', 'refs/replace/*:refs/replace/*']
             print(cmd2)
             if not dry_run:
                 exec(cmd2, cwd=path)
